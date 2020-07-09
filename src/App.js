1795be3153abae0a683cf3c93105aca66e6fb55b
Kaio Pomini <kaiopomini@gmail.com>
2020-07-09 10:49:03 -0300

    Search and spinner load

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
 import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search'
 import './App.css'
 
 
 const App = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
 
   useEffect(() => {
     const fetchItens = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
-      console.log(result.data)
       setItems(result.data)
       setIsLoading(false)
     }
 
     fetchItens()
-  }, [])
+  }, [query])
 
   return (
     <div className="Container">
       <Header />
-      <CharacterGrid isLoading={isLoading} />
+      <Search getQuery={(q) => setQuery(q)}/>
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
